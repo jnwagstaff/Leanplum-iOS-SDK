5e6e66b362a5390f3b73e6899ad8b71f44fa5eb4
Jacob Wagstaff <jnwagstaff@gmail.com>
2022-02-14 15:36:43 -0700

    fixes

--- Package.swift
+++ Package.swift
@@ -10,13 +10,14 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Leanplum",
-            url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/4.0.0/Leanplum.xcframework.zip",
+            url:
+                "https://github.com/jnwagstaff/Leanplum-iOS-SDK/releases/download/4.0.0/Leanplum.xcframework.zip",
             checksum: "4bfc5ae721fcbd47ec8d260c923457c43a8e68d3806b3d707689cdfbb0672411"
         ),
-       .target(
-           name: "LeanplumLocation",
-           path: "LeanplumSDKLocation",
-           publicHeadersPath: "LeanplumSDKLocation/include"
-       )
+        .target(
+            name: "LeanplumLocation",
+            path: "LeanplumSDKLocation",
+            publicHeadersPath: "LeanplumSDKLocation/include"
+        )
     ]
 )
